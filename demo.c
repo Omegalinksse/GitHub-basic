51ec73b4c57da8ff5513b5d8353ff40a8763c3a5
Omegalinksse <omegalinksse@gmail.com>
2026-04-20 05:26:56 +0100

    my first c practice

---
+++ demo.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+char n,a,m,i;
+int age;
+scanf("%c %c %c %c %d",&n,&a,&m,&i,&age);
+printf("my name is:%c%c%c%c\nmy age is: %d",n,a,m,i,age);
+return(0);
+}
